195bbb28c4ff460bf58d4b438c31cc8c91f909ca
Rafael Fonseca Hermes Azevedo <rafael.azevedo.dev@gmail.com>
2025-04-12 19:42:23 -0300

    commit inicial

---
+++ src/main/java/br/com/clash_utilities/ClashUtilitiesApplication.java
@@ -0,0 +1,13 @@
+package br.com.clash_utilities;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClashUtilitiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClashUtilitiesApplication.class, args);
+	}
+
+}
