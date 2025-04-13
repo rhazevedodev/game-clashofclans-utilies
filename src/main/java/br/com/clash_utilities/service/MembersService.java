b30585f96c72ed52b65148f6f629261375c1489f
Rafael Fonseca Hermes Azevedo <rafael.azevedo.dev@gmail.com>
2025-04-12 21:56:03 -0300

    teste

---
+++ src/main/java/br/com/clash_utilities/service/MembersService.java
@@ -0,0 +1,11 @@
+package br.com.clash_utilities.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MembersService {
+
+    public String exportMembersToExcel(){
+        return "service ok";
+    }
+}
